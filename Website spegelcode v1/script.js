129784988e3d3a72bde7aafcf77350f232383c71
spegelcode <Leo.spegel.berg@gmail.com>
2024-09-02 22:25:37 +0200

    carousel 50%

--- Website spegelcode v1/script.js
+++ Website spegelcode v1/script.js
@@ -1,13 +1,13 @@
 document.getElementById('scroll-left').addEventListener('click', function() {
   document.getElementById('scrollable-div').scrollBy({
-    left: -150,
-    behavior: 'smooth'
+      left: -150,
+      behavior: 'smooth'
   });
 });
 
 document.getElementById('scroll-right').addEventListener('click', function() {
   document.getElementById('scrollable-div').scrollBy( { 
-    left: 150,
-    behavior: 'smooth'
+      left: 150,
+      behavior: 'smooth'
   });
 });
